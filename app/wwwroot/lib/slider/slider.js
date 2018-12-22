6e6ff2ae4296f0c6574fefb784c2eaf161cb8f33
Samuel <samuel.lachman@futuresolutionservices.com>
2018-12-22 01:05:48 +0100

    Mobile visual

--- app/wwwroot/lib/slider/slider.js
+++ app/wwwroot/lib/slider/slider.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
     var slider = null;
-    $('.slider .slider_point').on('mousedown', function (e) {
+    $('.slider .slider_point').on('mousedown touchstart', function (e) {
         var sliderPoint = $(this);
         slider = { object: sliderPoint, x: e.originalEvent.clientX, margin: parseInt(sliderPoint.css('left')) };
     });
 
-    $('body').on('mousemove', function (e) {
+    $('body').on('mousemove touchmove', function (e) {
         if (slider != null) {
             var parentWidth = parseInt(slider.object.parent().css('width'));
             var xDiff = e.originalEvent.clientX - slider['x'];
             var currentMargin = slider['margin'] + xDiff;
 
             // is in range
-            if (currentMargin > -5 && currentMargin < (parentWidth - 5)) {
+            if (currentMargin > -15 && currentMargin < (parentWidth - 15)) {
                 // move
                 slider['object'].css('left', currentMargin);
 
@@ -47,11 +47,11 @@ $(document).ready(function () {
         if (thisObject.val() != '') {
             var field = parseInt(source.css('width')) / (max - min);
             var point = source.find('.slider_point');
-            point.css('left', (field * value) - 5);
+            point.css('left', (field * value) - 15);
         }
     });
 
-    $('body').on('mouseup', function (e) {
+    $('body').on('mouseup touchend', function (e) {
         if (slider != null)
             slider = null;
     });
