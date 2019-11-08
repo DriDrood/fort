9d51a7e0263ef55412e42e7b97454271e001cd39
Samuel Lachman <samuel.lachman@futuresolutionservices.com>
2019-11-08 13:13:21 +0100

    FE: don't go to future

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -81,7 +81,7 @@ export default new Vuex.Store({
     }
   },
   getters: {
-    currentTurnId: (state) => state.turns.length,
+    currentTurnId: (state) => state.turns.length - 1,
     distinctRoads: (state) => {
       let result = [];
       Object.keys(state.roads).forEach(id => {
